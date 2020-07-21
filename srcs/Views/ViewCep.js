bbc86f9cd7b437fcc389abe11a43beb23c21c47a
Paula Gomes <gomes.pcristina@gmail.com>
2020-07-21 08:37:52 -0300

    requisicao de CEP MVC

---
+++ srcs/Views/ViewCep.js
@@ -0,0 +1,27 @@
+class ViewCep{
+
+    constructor(){
+
+        throw new Error (" Class without instancies");
+    }
+
+    static displayAdress(ZipObj){
+
+      let streetView=document.querySelector("#street");
+      let numberView=document.querySelector("#number");
+      let cityView=document.querySelector("#city");
+      let stateView=document.querySelector("#state");
+      let districtView=document.querySelector("#district");
+
+      streetView.value=ZipObj.street;
+      numberView.value=ZipObj.complement;
+      cityView.value=ZipObj.city;
+      stateView.value=ZipObj.state;
+      districtView.value=ZipObj.district;   
+
+
+    }
+
+
+}
+
