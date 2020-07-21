62e9f5e39481bb2ac80edc58048cac0a0f0943e5
Paula Gomes <gomes.pcristina@gmail.com>
2020-07-21 08:37:11 -0300

    requisicao de CEP MVC

---
+++ srcs/Controllers/ControllerCep.js
@@ -0,0 +1,40 @@
+class ControllerCep{
+
+    constructor(){
+
+        throw new Error (" Class without instancies");
+
+    }
+
+    static getAdress(urlZip){
+
+        let requestZip= new XMLHttpRequest();
+        requestZip.open("GET",urlZip);
+
+        requestZip.addEventListener("load", () =>{
+
+            if (requestZip.status!=200)
+            {
+                throw new Error ("Request not found");
+            }
+
+            let requestZipObj=JSON.parse(requestZip.responseText);
+            
+            let completeAdress= new ModelCep(requestZipObj.logradouro,
+                                             requestZipObj.complemento,
+                                             requestZipObj.localidade,
+                                             requestZipObj.uf,
+                                             requestZipObj.bairro)
+            
+
+            console.log(completeAdress);
+            ViewCep.displayAdress(completeAdress.returnZip());           
+        
+
+        });
+
+        requestZip.send();
+
+    }
+
+}
