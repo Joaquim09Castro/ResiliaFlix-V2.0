2c395f9bd619febe85a9cd0900bb39c22b4ad37f
Joaquim Castro <joaquimcastro2909@gmail.com>
2020-07-25 14:23:48 -0300

    Modal do carrossel implementado

---
+++ srcs/javascript/carouselMovie.js
@@ -0,0 +1,28 @@
+carouselInfo = {
+  options: document.querySelectorAll("#carousel-item"),
+  carouselModal: $("#carousel-modal-body")
+};
+
+carouselInfo.options[0].onclick = () => CarouselModalController.search("tt4154796");
+
+carouselInfo.options[1].onclick = () => CarouselModalController.search("tt0816692");
+
+carouselInfo.options[2].onclick = () => CarouselModalController.search("tt6751668");
+
+carouselInfo.options[3].onclick = () => CarouselModalController.search("tt7286456");
+
+carouselInfo.options[4].onclick = () => CarouselModalController.search("tt5311514");
+
+carouselInfo.options[5].onclick = () => CarouselModalController.search("tt0120382");
+
+carouselInfo.options[6].onclick = () => CarouselModalController.search("tt1670345");
+
+carouselInfo.options[7].onclick = () => CarouselModalController.search("tt4972582");
+
+carouselInfo.options[8].onclick = () => CarouselModalController.search("tt2380307");
+
+carouselInfo.options[9].onclick = () => CarouselModalController.search("tt0325980");
+
+carouselInfo.options[10].onclick = () => CarouselModalController.search("tt2066051");
+
+carouselInfo.options[11].onclick = () => CarouselModalController.search("tt1375666");
