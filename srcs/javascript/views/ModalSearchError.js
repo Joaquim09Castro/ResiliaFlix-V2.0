346ab9762c461f1de5b55eb3cf872126cf79e3ee
Joaquim Castro <joaquimcastro2909@gmail.com>
2020-07-25 14:47:54 -0300

    ModalSearchError atualzado

--- srcs/javascript/views/ModalSearchError.js
+++ srcs/javascript/views/ModalSearchError.js
@@ -6,15 +6,9 @@ class ModalSearchError {
   template() {
     return `
     <div class="resultArea d-flex justify-content-center p-md-1 m-md-1">
-    <img class="poster p-3" id="poster" src="srcs/assets/placeholder_cover.jpg">
+    <img class="poster p-3" id="poster" src="srcs/assets/Popcorn.png">
     <div class="memberSearch m-md-3 p-md-3">
-      <h3 class="mb-5" id="title"><b>Title</b></h3>
-      <p id="year"><b>YEAR</b>: Year</p>
-      <p id="director"><b>DIRECTOR</b>: Director</p>
-      <p id="runtime"><b>RUNTIME</b>: Runtime</p>
-      <p id="actors"><b>RATING</b>: Rating</p>
-      <p id="release"><b>RELEASE DATE</b>: Release</p>
-      <p id="synopsis"><b>SYNOPSIS</b>: Synopsis</p>
+      <h3 class="mb-5 text-center" id="title"><b>Movie Not Found</b></h3>
   </div>
     `;
   }
