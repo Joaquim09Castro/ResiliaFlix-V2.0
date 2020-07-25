c1b0d6d69e6508977f69126b43584eca086732d1
Joaquim Castro <joaquimcastro2909@gmail.com>
2020-07-25 17:28:43 -0300

    Interação com o formulário consertada

--- srcs/javascript/modalSearch.js
+++ srcs/javascript/modalSearch.js
@@ -3,6 +3,17 @@ const modalInfo = {
   btnSearch: $("#button-search"),
   searchField: $("#movie-search"),
   modal: $("#modal-body"),
+  form: $("#form-search")
+};
+
+modalInfo.form.onsubmit = (e) => {
+  e.preventDefault();
+  if (modalInfo.searchField.value != "") {
+    ModalSearchController.search(modalInfo.searchField);
+  } else {
+    let modalError = new ModalSearchError(modalInfo.modal);
+    modalError.update();
+  }
 };
 
 modalInfo.btnSearch.onclick = () => {
