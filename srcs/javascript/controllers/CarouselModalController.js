087b55aaff0541182670d0b0300ed6c7d90d45bf
Joaquim Rodrigo Moraes de Castro <65615491+Joaquim09Castro@users.noreply.github.com>
2020-07-25 23:26:38 -0300

    Update CarouselModalController.js

--- srcs/javascript/controllers/CarouselModalController.js
+++ srcs/javascript/controllers/CarouselModalController.js
@@ -6,16 +6,12 @@ class CarouselModalController {
   static search(id) {
     let xhr = new XMLHttpRequest();
     xhr.open("GET", `${modalInfo.url}&i=${id}`);
-
     xhr.onload = () => {
       let responseJson = JSON.parse(xhr.responseText);
-
       let responseModel = new ModalSearchModel(responseJson);
       let responseView = new ModalSearchView(carouselInfo.carouselModal);
-      
       responseView.update(responseModel.data);
     };
-
     xhr.send();
   }
 }
