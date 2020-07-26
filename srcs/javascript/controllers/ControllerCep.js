8d1fadad108d90eef367214a1e367ce7e4cba72a
Paula Gomes <gomes.pcristina@gmail.com>
2020-07-25 22:05:37 -0300

    validando rg, email, senha e exibindo msgs de erro

--- srcs/javascript/controllers/ControllerCep.js
+++ srcs/javascript/controllers/ControllerCep.js
@@ -1,30 +1,29 @@
 class ControllerCep {
-    constructor() {
-        throw new Error(" Class without instancies");
-    }
+  constructor() {
+    throw new Error(" Class without instancies");
+  }
 
-    static getAdress(urlZip) {
-        let requestZip = new XMLHttpRequest();
-        requestZip.open("GET", urlZip);
+  static getAdress(urlZip) {
+    const requestZip = new XMLHttpRequest();
+    requestZip.open("GET", urlZip);
 
-        requestZip.addEventListener("load", () => {
-            if (requestZip.status != 200) {
-                throw new Error("Request not found");
-            }
+    requestZip.addEventListener("load", () => {
+      if (requestZip.status != 200) {
+        throw new Error("Request not found");
+      }
+      const requestZipObj = JSON.parse(requestZip.responseText);
 
-            let requestZipObj = JSON.parse(requestZip.responseText);
+      const completeAdress = new ModelCep(
+        requestZipObj.logradouro,
+        requestZipObj.complemento,
+        requestZipObj.localidade,
+        requestZipObj.uf,
+        requestZipObj.bairro
+      );
+      console.log(completeAdress);
+      ViewCep.displayAdress(completeAdress.returnZip());
+    });
 
-            let completeAdress = new ModelCep(
-                requestZipObj.logradouro,
-                requestZipObj.complemento,
-                requestZipObj.localidade,
-                requestZipObj.uf,
-                requestZipObj.bairro
-            );
-
-            ViewCep.displayAdress(completeAdress.returnZip());
-        });
-
-        requestZip.send();
-    }
+    requestZip.send();
+  }
 }
