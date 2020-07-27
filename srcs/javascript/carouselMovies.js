04e195ee6afeb03e5ea21c52c6d76ffe417266da
Jarom Antony Aidukaitis <jaaidukaitis@gmail.com>
2020-07-27 03:21:55 -0300

    modal para movies.html

---
+++ srcs/javascript/carouselMovies.js
@@ -0,0 +1,220 @@
+carouselInfo = {
+	options: document.querySelectorAll("#carousel-item"),
+	carouselModal: $("#carousel-modal-body"),
+};
+
+carouselInfo.options[0].onclick = () =>
+	CarouselModalController.search("tt4154796");
+
+carouselInfo.options[1].onclick = () =>
+	CarouselModalController.search("tt2294629");
+
+carouselInfo.options[2].onclick = () =>
+	CarouselModalController.search("tt1564367");
+
+carouselInfo.options[3].onclick = () =>
+	CarouselModalController.search("tt0325980");
+
+carouselInfo.options[4].onclick = () =>
+	CarouselModalController.search("tt0120338");
+
+carouselInfo.options[5].onclick = () =>
+	CarouselModalController.search("tt0499549");
+
+carouselInfo.options[6].onclick = () =>
+	CarouselModalController.search("tt2771200");
+
+carouselInfo.options[7].onclick = () =>
+	CarouselModalController.search("tt0167260");
+
+carouselInfo.options[8].onclick = () =>
+	CarouselModalController.search("tt0137523");
+
+carouselInfo.options[9].onclick = () =>
+	CarouselModalController.search("tt0468569");
+
+carouselInfo.options[10].onclick = () =>
+	CarouselModalController.search("tt3521164");
+
+carouselInfo.options[11].onclick = () =>
+	CarouselModalController.search("tt0926084");
+
+carouselInfo.options[12].onclick = () =>
+	CarouselModalController.search("tt2802144");
+
+carouselInfo.options[13].onclick = () =>
+	CarouselModalController.search("tt2066051");
+
+carouselInfo.options[14].onclick = () =>
+	CarouselModalController.search("tt6751668");
+
+carouselInfo.options[15].onclick = () =>
+	CarouselModalController.search("tt0073629");
+
+carouselInfo.options[16].onclick = () =>
+	CarouselModalController.search("tt4972582");
+
+carouselInfo.options[17].onclick = () =>
+	CarouselModalController.search("tt0816692");
+
+carouselInfo.options[18].onclick = () =>
+	CarouselModalController.search("tt7286456");
+
+carouselInfo.options[19].onclick = () =>
+	CarouselModalController.search("tt8503618");
+
+carouselInfo.options[20].onclick = () =>
+	CarouselModalController.search("tt1049413");
+
+carouselInfo.options[21].onclick = () =>
+	CarouselModalController.search("tt0414387");
+
+carouselInfo.options[22].onclick = () =>
+	CarouselModalController.search("tt1670345");
+
+carouselInfo.options[23].onclick = () =>
+	CarouselModalController.search("tt0230600");
+
+carouselInfo.options[24].onclick = () =>
+	CarouselModalController.search("tt1315981");
+
+carouselInfo.options[25].onclick = () =>
+	CarouselModalController.search("tt0993846");
+
+carouselInfo.options[26].onclick = () =>
+	CarouselModalController.search("tt1188729");
+
+carouselInfo.options[27].onclick = () =>
+	CarouselModalController.search("tt5990474");
+
+carouselInfo.options[28].onclick = () =>
+	CarouselModalController.search("tt2101569");
+
+carouselInfo.options[29].onclick = () =>
+	CarouselModalController.search("tt5311514");
+
+carouselInfo.options[30].onclick = () =>
+	CarouselModalController.search("tt1374992");
+
+carouselInfo.options[31].onclick = () =>
+	CarouselModalController.search("tt1985019");
+
+carouselInfo.options[32].onclick = () =>
+	CarouselModalController.search("tt3416532");
+
+carouselInfo.options[33].onclick = () =>
+	CarouselModalController.search("tt5323662");
+
+carouselInfo.options[34].onclick = () =>
+	CarouselModalController.search("tt0120382");
+
+carouselInfo.options[35].onclick = () =>
+	CarouselModalController.search("tt1289403");
+
+carouselInfo.options[36].onclick = () =>
+	CarouselModalController.search("tt4154796");
+
+carouselInfo.options[37].onclick = () =>
+	CarouselModalController.search("tt2294629");
+
+carouselInfo.options[38].onclick = () =>
+	CarouselModalController.search("tt1564367");
+
+carouselInfo.options[39].onclick = () =>
+	CarouselModalController.search("tt0325980");
+
+carouselInfo.options[40].onclick = () =>
+	CarouselModalController.search("tt0120338");
+
+carouselInfo.options[41].onclick = () =>
+	CarouselModalController.search("tt0499549");
+
+carouselInfo.options[42].onclick = () =>
+	CarouselModalController.search("tt2771200");
+
+carouselInfo.options[43].onclick = () =>
+	CarouselModalController.search("tt0167260");
+
+carouselInfo.options[44].onclick = () =>
+	CarouselModalController.search("tt0137523");
+
+carouselInfo.options[45].onclick = () =>
+	CarouselModalController.search("tt0468569");
+
+carouselInfo.options[46].onclick = () =>
+	CarouselModalController.search("tt3521164");
+
+carouselInfo.options[47].onclick = () =>
+	CarouselModalController.search("tt0926084");
+
+carouselInfo.options[48].onclick = () =>
+	CarouselModalController.search("tt2802144");
+
+carouselInfo.options[49].onclick = () =>
+	CarouselModalController.search("tt2066051");
+
+carouselInfo.options[50].onclick = () =>
+	CarouselModalController.search("tt6751668");
+
+carouselInfo.options[51].onclick = () =>
+	CarouselModalController.search("tt0073629");
+
+carouselInfo.options[52].onclick = () =>
+	CarouselModalController.search("tt4972582");
+
+carouselInfo.options[53].onclick = () =>
+	CarouselModalController.search("tt0816692");
+
+carouselInfo.options[54].onclick = () =>
+	CarouselModalController.search("tt7286456");
+
+carouselInfo.options[55].onclick = () =>
+	CarouselModalController.search("tt8503618");
+
+carouselInfo.options[56].onclick = () =>
+	CarouselModalController.search("tt1049413");
+
+carouselInfo.options[57].onclick = () =>
+	CarouselModalController.search("tt0414387");
+
+carouselInfo.options[58].onclick = () =>
+	CarouselModalController.search("tt1670345");
+
+carouselInfo.options[59].onclick = () =>
+	CarouselModalController.search("tt0230600");
+
+carouselInfo.options[60].onclick = () =>
+	CarouselModalController.search("tt1315981");
+
+carouselInfo.options[61].onclick = () =>
+	CarouselModalController.search("tt0993846");
+
+carouselInfo.options[62].onclick = () =>
+	CarouselModalController.search("tt1188729");
+
+carouselInfo.options[63].onclick = () =>
+	CarouselModalController.search("tt5990474");
+
+carouselInfo.options[64].onclick = () =>
+	CarouselModalController.search("tt2101569");
+
+carouselInfo.options[65].onclick = () =>
+	CarouselModalController.search("tt5311514");
+
+carouselInfo.options[66].onclick = () =>
+	CarouselModalController.search("tt1374992");
+
+carouselInfo.options[67].onclick = () =>
+	CarouselModalController.search("tt1985019");
+
+carouselInfo.options[68].onclick = () =>
+	CarouselModalController.search("tt3416532");
+
+carouselInfo.options[69].onclick = () =>
+	CarouselModalController.search("tt5323662");
+
+carouselInfo.options[70].onclick = () =>
+	CarouselModalController.search("tt0120382");
+
+carouselInfo.options[71].onclick = () =>
+	CarouselModalController.search("tt1289403");
